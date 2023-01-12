02e188ccb99ebb75a675c4cb8cfbad73cae96f4a
Clément Lapasset <clement.lapasset@gmail.com>
2023-01-12 13:18:21 +0100

    Images resized

--- src/pages/project/{mdx.frontmatter__slug}.js
+++ src/pages/project/{mdx.frontmatter__slug}.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { graphql, Link } from 'gatsby'
-import { GatsbyImage, StaticImage, getImage } from 'gatsby-plugin-image'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 
 import Layout from '../../components/global/Layout'
 import Seo from '../../components/global/Seo'
@@ -10,46 +10,40 @@ import "../../../css/main.css"
 
 const ProjectPage = ({ data, children }) => {
   const mobileImg = getImage(data.mdx.frontmatter.mobile_img)
-  const desktopImg1 = getImage(data.mdx.frontmatter.desktop_img_1)
-  const desktopImg2 = getImage(data.mdx.frontmatter.desktop_img_2)
+  const desktopImg = getImage(data.mdx.frontmatter.desktop_img)
 
   const url = data.mdx.frontmatter.url
   return (
 
     <Layout pageTitle={data.mdx.frontmatter.number}>
-      <div className="project-page" style={{
-        backgroundColor: data.mdx.frontmatter.bg_color
-      }}>
-        <div className="left-container">
-          <GatsbyImage
-            className='mobile-img'
-            image={mobileImg}
-            alt={data.mdx.frontmatter.mobile_img_alt}
-          />
-          <div className="project-text">
-            <h2 className='project-title'>{data.mdx.frontmatter.title}</h2>
-            <div className="project-description">{children}</div>
+      <div className="project-page" style={{ backgroundColor: data.mdx.frontmatter.bg_color }}>
+        <div className="grid-container">
+          <div className="mobile-img">
+            <GatsbyImage
+              className='project-img'
+              image={mobileImg}
+              alt={data.mdx.frontmatter.mobile_img_alt}
+            />
+          </div>
+          <h2 className="project-title">{data.mdx.frontmatter.title}</h2>
+          <div className="project-description">
+            {children}
             <div className="row">
               {url ? (
                 <Link className='project-link' to={data.mdx.frontmatter.url} target="_blank">Visiter le site</Link>
               ) : (
-                <div>Site non-accessible</div>
+                null
               )}
               <Link className='next-project-link' to='/'>Projet suivant</Link>
             </div>
           </div>
-        </div>
-        <div className="right-container">
-          <GatsbyImage
-            className='desktop-img'
-            image={desktopImg1}
-            alt={data.mdx.frontmatter.desktop_img_1_alt}
-          />
-          <GatsbyImage
-            className='desktop-img'
-            image={desktopImg2}
-            alt={data.mdx.frontmatter.desktop_img_2_alt}
-          />
+          <div className="desktop-img">
+            <GatsbyImage
+              className='project-img'
+              image={desktopImg}
+              alt={data.mdx.frontmatter.desktop_img_alt}
+            />
+          </div>
         </div>
       </div>
     </Layout >
@@ -60,34 +54,28 @@ export const Head = ({ data }) => <Seo title={data.mdx.frontmatter.client} />
 
 export const query = graphql`
   query ($id: String) {
-    mdx(id: { eq: $id } ) {
-      frontmatter {
-        client
-        title
-        url
-        number
-        bg_color
-        mobile_img {
-          childImageSharp {
-            gatsbyImageData
-          }
+  mdx(id: {eq: $id}) {
+    frontmatter {
+      client
+      title
+      url
+      number
+      bg_color
+      mobile_img {
+        childImageSharp {
+          gatsbyImageData
         }
-        mobile_img_alt
-        desktop_img_1 {
-          childImageSharp {
-            gatsbyImageData
-          }
-        }
-        desktop_img_1_alt
-        desktop_img_2 {
-          childImageSharp {
-            gatsbyImageData
-          }
-        }
-        desktop_img_2_alt
       }
+      mobile_img_alt
+      desktop_img {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+      desktop_img_alt
     }
   }
+}
 `
 
 export default ProjectPage
